a952332995943422d50daa6de7ff462aa4900ab3
Pedro Bufulin <pedrohba18@gmail.com>
2020-04-02 16:46:07 -0300

    background da aplicação

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { View } from 'react-native';
 
-// import { Container } from './styles';
+import Background from '~/components/Background';
 
 export default function SignUp() {
-    return <View />;
+    return <Background />;
 }
