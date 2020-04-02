4ae159ac95d5bb4ab4a914bee5b814f4cfb711bb
Pedro Bufulin <pedrohba18@gmail.com>
2020-04-02 16:05:03 -0300

    rotas de sign in e sign out

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,10 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import colors from './styles/colors';
 
-import Home from './pages/Home';
-import Cart from './pages/Cart';
+import SignIn from '~/pages/SignIn';
+import SignUp from '~/pages/SignUp';
+
 import ShoesHeader from './components/ShoesHeader';
 
 const Stack = createStackNavigator();
@@ -16,12 +16,12 @@ function Routes() {
     return (
         <NavigationContainer>
             <Stack.Navigator
-                screenOptions={{
+            /*       screenOptions={{
                     header: () => <ShoesHeader />,
-                }}
+                }} */
             >
-                <Stack.Screen name="Home" component={Home} />
-                <Stack.Screen name="Cart" component={Cart} />
+                <Stack.Screen name="sign in" component={SignIn} />
+                <Stack.Screen name="sign up" component={SignUp} />
             </Stack.Navigator>
         </NavigationContainer>
     );
