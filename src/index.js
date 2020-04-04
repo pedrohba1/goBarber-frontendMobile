64ef0fdcce9e0f2814ee0067ab7bdf2b58c1ccbf
Pedro Bufulin <pedrohba18@gmail.com>
2020-04-04 15:39:57 -0300

    status bar configurada

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 // In App.js in a new project
 
 import * as React from 'react';
+import { StatusBar } from 'react-native';
 import Routes from '~/routes';
 
 function App() {
     return (
         <>
+            <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
             <Routes />
         </>
     );
