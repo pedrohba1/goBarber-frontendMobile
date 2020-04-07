f04dbcd79f6aa84f875cde80e96c18a4def270ce
Pedro Bufulin <pedrohba18@gmail.com>
2020-04-07 19:51:46 -0300

    listagem de agendamentos

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,8 +2,12 @@ import React, { useState } from 'react';
 import { View, TextInput } from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Container, Title, List } from './styles';
 
 import Background from '~/components/Background';
+import Appointment from '~/components/Appointment';
+
+const data = [1, 2, 3, 4, 5, 6];
 
 export default function Dashboard({ navigation }) {
     navigation.setOptions({
@@ -15,7 +19,15 @@ export default function Dashboard({ navigation }) {
 
     return (
         <Background>
-            <TextInput>testeee</TextInput>
+            <Container>
+                <Title>Agendamentos</Title>
+
+                <List
+                    data={data}
+                    renderItem={({ item }) => <Appointment data={item} />}
+                    keyExtractor={item => String(item)}
+                />
+            </Container>
         </Background>
     );
 }
