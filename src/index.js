9681c8e751d9c9238d59f291140cbc193f739b51
Pedro Bufulin <pedrohba18@gmail.com>
2020-04-06 23:46:31 -0300

    react redux, sagas e a configuração do reactotorn para eles

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 // In App.js in a new project
 
 import * as React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 import { StatusBar } from 'react-native';
-import Routes from '~/routes';
 
 import './config/ReactotronConfig';
 
+import { store, persistor } from './store';
+import Routes from '~/routes';
+
 function App() {
     return (
-        <>
-            <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-            <Routes />
-        </>
+        <Provider store={store}>
+            <PersistGate persistor={persistor}>
+                <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+                <Routes />
+            </PersistGate>
+        </Provider>
     );
 }
 
