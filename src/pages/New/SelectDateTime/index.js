d5886a9a33dce7400857c45ad5630789c0fc46ec
Pedro Bufulin <pedrohba18@gmail.com>
2020-04-09 17:11:08 -0300

    seleção de data

--- src/pages/New/SelectDateTime/index.js
+++ src/pages/New/SelectDateTime/index.js
@@ -1,14 +1,17 @@
-import React, { useLayoutEffect } from 'react';
+import React, { useLayoutEffect, useState } from 'react';
 import { TouchableOpacity } from 'react-native';
 import { CommonActions } from '@react-navigation/native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '~/components/Background';
+import DateInput from '~/components/DateInput';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function SelectDateTime({ navigation }) {
+    const [date, setDate] = useState(new Date());
+
     useLayoutEffect(() => {
         navigation.setOptions({
             headerLeft: () => (
@@ -23,5 +26,11 @@ export default function SelectDateTime({ navigation }) {
         });
     }, []);
 
-    return <Background />;
+    return (
+        <Background>
+            <Container>
+                <DateInput date={date} onChange={setDate} />
+            </Container>
+        </Background>
+    );
 }
